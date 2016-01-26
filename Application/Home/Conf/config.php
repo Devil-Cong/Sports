61616a3c4e2a13fb12e7f6e669929b54d50ef319
Leon <liujy@xbed.com.cn>
2016-01-26 18:38:31 +0800

    init project

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'TMPL_L_DELIM'    => '<{',
+	'TMPL_R_DELIM'    => '}>',
+	'SHOW_PAGE_TRACE' => true, //显示调试面板
+	/*数据库配置*/
+	'DB_TYPE'         => 'mysql', // 数据库类型
+	'DB_HOST'         => '192.168.27.1', // 服务器地址
+	'DB_NAME'         => 'sports', // 数据库名
+	'DB_USER'         => 'root', // 用户名
+	'DB_PWD'          => '123456', // 密码
+	'DB_PORT'         => 3306, // 端口
+	'DB_PREFIX'       => 'sp_', // 数据库表前缀
+	'DB_CHARSET'      => 'utf8', // 字符集
+	'DB_DEBUG'        => TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
+);
